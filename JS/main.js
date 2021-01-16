bd19025df0bd218094958aedd794cc229c1491e3
itdesigntk <66639356+itdesigntk@users.noreply.github.com>
2021-01-15 19:30:26 -0600

    Landing Page With Navigation
    Finished Landing Page design

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+var expanded = false
+function expandViewOfSidebar () {
+    
+    if (expanded == true) {
+        var bar = document.getElementById('sideBarNav')
+        bar.style.right = null; this.style.transform = null
+        expanded = false
+        return;
+    }
+
+    var bar = document.getElementById('sideBarNav')
+    bar.style.right = '0'; this.style.transform = 'rotate(90deg)'
+    expanded = true
+}
+document.getElementById('logoFinal').addEventListener('click', expandViewOfSidebar)
+document.getElementById('hinterClose').addEventListener('click', function() {
+    document.getElementById('logoFinal').dispatchEvent(new Event('click'));
+})
